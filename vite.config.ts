a37de913af54cdbcb89a5190d4a1b70a3739484d
noxsub <mtsfisc@gmail.com>
2026-06-30 14:56:46 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  // Deixa esse campo aqui para a automação da Cloudflare não quebrar:
+  plugins: [],
   // @ts-ignore
   nitro: {
     preset: "cloudflare-pages"
